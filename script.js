a377e195152092e1299d31611ad925d5c6cf0542
Rajesh Choudhary <rajuvert18@gmail.com>
2022-12-31 11:39:49 +0530

    accordion toggle added to task

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const tasks = document.getElementsByClassName("tasks");
 // const task = document.querySelector(".task");
 const task  =  document.getElementsByClassName("task");
+// const input = document.getElementById("input");
 
+//    input.setAttribute("readonly" , "readonly");
+//    console.log(input);
 
 
 for(let  i = 0 ; i<tasks.length ;i++){
-    tasks[i].addEventListener("click",(e)=>{
-        e.preventDefault();
+    task[i].addEventListener("click",()=>{
+        // tasks[i].setAttribute('');
         tasks[i].classList.toggle("active");
     });
 }
